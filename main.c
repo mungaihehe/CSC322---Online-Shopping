5b7a2ed1e4a0d29aa1daad2781cd587d5499b9a6
devmungai <101387978+devmungai@users.noreply.github.com>
2022-04-26 21:27:20 +0300

    product name space bug

--- main.c
+++ main.c
@@ -5,82 +5,89 @@
 
 
 void app(struct Catalog* catalog) {
-    printf("Online shopping application. Select an action \n");
-    printf("1 - Add Product \t 2 - Search Catalog \t 3 - Display catalog \t 4 - Catalog size \t 5 - Purchase Item (make payment) \n");
-    int action = -1;
-    scanf("%d", &action);
+    int exit = 0;
+    while(exit != 1) {
+        printf("Online shopping application. Select an action \n");
+        printf(" 1 - Add Product \n 2 - Search Catalog \n 3 - Display catalog \n 4 - Catalog size \n 5 - Purchase Item (make payment) \n 6 - Exit \n");
+        int action = -1;
+        scanf("%d", &action);
 
-    switch(action) {
-    case 1: ;
-    {
-        struct Product* product = newProduct("", 0, "");
-        printf("Enter the product's name: ");
-        scanf("%s", product->name);
-        printf("\n");
-        printf("Enter the product's price: ");
-        scanf("%d", &product->price);
-        printf("\n");
-        int stock = 0;
-        printf("Enter the product's stock: ");
-        scanf("%d", &stock);
-        printf("\n");
-        struct CatalogItem* item = newCatalogItem(product, stock);
-        addToCatalog(item, catalog);
-        break;
-    }
-    case 2:
+        switch(action) {
+        case 1: ;
         {
-            printf("Enter product name: \n");
-            char name[20];
-            scanf("%s", name);
-            struct Product* p = newProduct(name, 0, "");
-            struct CatalogItem* item = searchCatalog(*p, *catalog);
-            if(item->stock == 0) printf("Product does not exist in the catalog \n");
-            else {
-                printf("Name: %s \t Price: %d \t Stock: %d \n", item->product->name, item->product->price, item->stock);
-            }
+            struct Product* product = newProduct("", 0, "");
+            printf("Enter the product's name: ");
+            scanf(" %[^\n]", product->name);
+            printf("\n");
+            printf("Enter the product's price: ");
+            scanf("%d", &product->price);
+            printf("\n");
+            int stock = 0;
+            printf("Enter the product's stock: ");
+            scanf("%d", &stock);
+            printf("\n");
+            struct CatalogItem* item = newCatalogItem(product, stock);
+            addToCatalog(item, catalog);
             break;
         }
-    case 3:
-        printf("\n Catalog Items \n");
-        for(int i = 0; i<catalog->catalogSize; i++) {
-            struct CatalogItem* item = catalog->items[i];
-            printf("Item name: %s \t Item price: %d \t Stock: %d \n", item->product->name, item->product->price, item->stock);
+        case 2:
+            {
+                printf("Enter product name: \n");
+                char name[20];
+                scanf("%s", name);
+                struct Product* p = newProduct(name, 0, "");
+                struct CatalogItem* item = searchCatalog(*p, *catalog);
+                if(item->stock == 0) printf("Product does not exist in the catalog \n");
+                else {
+                    printf("Name: %s \t Price: %d \t Stock: %d \n", item->product->name, item->product->price, item->stock);
+                }
+                break;
+            }
+        case 3:
+            for(int i = 0; i<catalog->catalogSize; i++) {
+                struct CatalogItem* item = catalog->items[i];
+                printf(" Catalog Item: %d\n", i);
+                printf(" Item name: %s \n Item price: %d \n Stock: %d \n\n", item->product->name, item->product->price, item->stock);
+            }
+            break;
+        case 4:
+            printf("Catalog size: %d \n", catalog->catalogSize);
+            break;
+        case 5: ;
+        {
+            struct Product* product = newProduct("", 0, "");
+            printf("Enter the product's name: ");
+            scanf("%s", product->name);
+            printf("\n");
+            int paymentAmount = 0;
+            printf("Enter payment amount: ");
+            scanf("%d", &paymentAmount);
+            struct Payment payment = *newPayment(paymentAmount);
+
+            struct Payment change = purchaseItem(*product, payment, catalog);
+            if(change.amount == payment.amount) {
+                printf("Could not complete purchase. Check that the item exists in the catalog AND that the payment amount is more than the product's price \n");
+            } else {
+                printf("Change amount: %d \n", change.amount);
+            }
+
+            break;
         }
-        break;
-    case 4:
-        printf("Catalog size: %d \n", catalog->catalogSize);
-        break;
-    case 5: ;
-    {
-        struct Product* product = newProduct("", 0, "");
-        printf("Enter the product's name: ");
-        scanf("%s", product->name);
+        case 6:
+            exit = 1;
+            break;
+        default:
+            printf("Invalid selection");
+            break;
+        }
         printf("\n");
-        int paymentAmount = 0;
-        printf("Enter payment amount: ");
-        scanf("%d", &paymentAmount);
-        struct Payment payment = *newPayment(paymentAmount);
-
-        struct Payment change = purchaseItem(*product, payment, catalog);
-        if(change.amount == payment.amount) {
-            printf("Could not complete purchase. Check that the item exists in the catalog AND that the payment amount is more than the product's price \n");
-        } else {
-            printf("Change amount: %d \n", change.amount);
-        }
-
-        break;
     }
-    default:
-        printf("Invalid selection");
-        break;
-    }
-    app(catalog);
 }
 
 int main()
 {
     struct Catalog* catalog = newCatalog();
+
     app(catalog);
     return 0;
 }
